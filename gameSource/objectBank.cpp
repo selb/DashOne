d14e56ce1f64557421937ac1bdf38a7d568c226f
risvh <67486979+risvh@users.noreply.github.com>
2020-08-28 07:19:32 +0800

    Reverting this change as it changes how auto-generated obejcts (e.g. useDummies) are numbered.

--- gameSource/objectBank.cpp
+++ gameSource/objectBank.cpp
@@ -3522,8 +3522,7 @@ int addObject( const char *inDescription,
         }
     
 
-    if( ! inNoWriteToFile && 
-        objectsDir.exists() && objectsDir.isDirectory() ) {
+    if( objectsDir.exists() && objectsDir.isDirectory() ) {
         
         char *fileName = autoSprintf( "%d.txt", newID );
 
