e5f4fde47a15ffe7519c14e0df92ea1d632787b3
risvh <67486979+risvh@users.noreply.github.com>
2022-05-03 00:16:41 +0800

    This seems to have caused some crashes...

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -20432,7 +20432,6 @@ void LivingLifePage::step() {
                                 
                             if( existing->tag != NULL ) {
                                 delete [] existing->tag;
-                                existing->tag = NULL;
                                 }
                             
                             char *firstSpace = strstr( lines[i], " " );
@@ -20446,6 +20445,9 @@ void LivingLifePage::step() {
                                     char *tagStart = &( firstPlus[0] );
                                     existing->tag = stringDuplicate( tagStart );
                                     (firstPlus - 1)[0] = '\0';
+                                    }
+                                else {
+                                    existing->tag = stringDuplicate( "" );
                                     }
 
                                 char *nameStart = &( firstSpace[1] );
