c84253abf4f88d4c08f2a6ebb5f9f731bda9555f
risvh <67486979+risvh@users.noreply.github.com>
2022-06-07 01:54:34 +0800

    Fixed new font space width

--- gameSource/game.cpp
+++ gameSource/game.cpp
@@ -446,7 +446,7 @@ void initDrawString( int inWidth, int inHeight ) {
 	hetuwSetViewSize();
 
 
-    mainFont = new Font( getNewFontTGAFileName(), 3, 16, false, 16 );
+    mainFont = new Font( getNewFontTGAFileName(), 3, 4, false, 16 );
     // mainFont = new Font( getFontTGAFileName(), 6, 16, false, 16 );
     mainFont->setMinimumPositionPrecision( 1 );
     oldMainFont = new Font( getFontTGAFileName(), 6, 6, false, 16 );
