1153ae59105a62b003c10dfe4a3652661c50ce83
risvh <67486979+risvh@users.noreply.github.com>
2022-06-28 01:08:16 +0800

    Clear button for seed field

--- gameSource/DropdownList.h
+++ gameSource/DropdownList.h
@@ -118,6 +118,9 @@ class DropdownList : public PageComponent, public ActionListenerList {
         
         // defaults to off
         void usePasteShortcut( char inShortcutOn );
+		
+        // defaults to off
+        void useClearButton( char inClearButtonOn );
         
 
         
@@ -212,6 +215,8 @@ class DropdownList : public PageComponent, public ActionListenerList {
 		char isInsideTextBox( float inX, float inY );
 		bool nearRightEdge;
 		char isNearRightEdge( float inX, float inY );
+		bool mUseClearButton;
+		bool onClearButton;
         
         int mCursorPosition;
         
